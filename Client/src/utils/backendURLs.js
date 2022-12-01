81bdaf3642c049e3163981513bb22016dabd3e7b
jai2201 <jai_s@me.iitr.ac.in>
2022-12-02 00:51:49 +0530

    client: Change URLs to use hosted IP.

--- Client/src/utils/backendURLs.js
+++ Client/src/utils/backendURLs.js
@@ -1,53 +1,55 @@
-export const LOGIN_URL = 'http://127.0.0.1:8080/api/post/login';
-export const GET_ALL_USERS = 'http://127.0.0.1:8080/api/get/users';
-export const GET_USER_DETAILS = 'http://127.0.0.1:8080/api/get/user_details';
-export const ADD_NEW_USER = 'http://127.0.0.1:8080/api/post/user';
-export const EDIT_A_USER = 'http://127.0.0.1:8080/api/put/user';
-export const DELETE_A_USER = 'http://127.0.0.1:8080/api/delete/user';
-export const GET_ALL_EXPENSES = 'http://127.0.0.1:8080/api/get/expenses';
-export const ADD_NEW_EXPENSE = 'http://127.0.0.1:8080/api/post/expense';
+export const LOGIN_URL = 'http://15.206.100.165:8080/api/post/login';
+export const GET_ALL_USERS = 'http://15.206.100.165:8080/api/get/users';
+export const GET_USER_DETAILS =
+  'http://15.206.100.165:8080/api/get/user_details';
+export const ADD_NEW_USER = 'http://15.206.100.165:8080/api/post/user';
+export const EDIT_A_USER = 'http://15.206.100.165:8080/api/put/user';
+export const DELETE_A_USER = 'http://15.206.100.165:8080/api/delete/user';
+export const GET_ALL_EXPENSES = 'http://15.206.100.165:8080/api/get/expenses';
+export const ADD_NEW_EXPENSE = 'http://15.206.100.165:8080/api/post/expense';
 export const GET_EXPENSE_DETAILS =
-  'http://127.0.0.1:8080/api/get/expense_details';
-export const EDIT_A_EXPENSE = 'http://127.0.0.1:8080/api/put/expense';
-export const DELETE_A_EXPENSE = 'http://127.0.0.1:8080/api/delete/expense';
-export const GET_ALL_PARTNERS = 'http://127.0.0.1:8080/api/get/partners';
+  'http://15.206.100.165:8080/api/get/expense_details';
+export const EDIT_A_EXPENSE = 'http://15.206.100.165:8080/api/put/expense';
+export const DELETE_A_EXPENSE = 'http://15.206.100.165:8080/api/delete/expense';
+export const GET_ALL_PARTNERS = 'http://15.206.100.165:8080/api/get/partners';
 export const GET_PARTNER_DETAILS =
-  'http://127.0.0.1:8080/api/get/partner_details';
-export const ADD_NEW_PARTNER = 'http://127.0.0.1:8080/api/post/partner';
-export const EDIT_A_PARTNER = 'http://127.0.0.1:8080/api/put/partner';
-export const DELETE_A_PARTNER = 'http://127.0.0.1:8080/api/delete/partner';
-export const GET_ALL_PROJECTS = 'http://127.0.0.1:8080/api/get/projects';
+  'http://15.206.100.165:8080/api/get/partner_details';
+export const ADD_NEW_PARTNER = 'http://15.206.100.165:8080/api/post/partner';
+export const EDIT_A_PARTNER = 'http://15.206.100.165:8080/api/put/partner';
+export const DELETE_A_PARTNER = 'http://15.206.100.165:8080/api/delete/partner';
+export const GET_ALL_PROJECTS = 'http://15.206.100.165:8080/api/get/projects';
 export const GET_PROJECT_DETAILS =
-  'http://127.0.0.1:8080/api/get/project_details';
-export const ADD_NEW_PROJECT = 'http://127.0.0.1:8080/api/post/project';
-export const EDIT_A_PROJECT = 'http://127.0.0.1:8080/api/put/project';
-export const DELETE_A_PROJECT = 'http://127.0.0.1:8080/api/delete/project';
+  'http://15.206.100.165:8080/api/get/project_details';
+export const ADD_NEW_PROJECT = 'http://15.206.100.165:8080/api/post/project';
+export const EDIT_A_PROJECT = 'http://15.206.100.165:8080/api/put/project';
+export const DELETE_A_PROJECT = 'http://15.206.100.165:8080/api/delete/project';
 export const GET_ALL_EMPLOYEES_FOR_A_PROJECT =
-  'http://127.0.0.1:8080/api/get/employees/project';
+  'http://15.206.100.165:8080/api/get/employees/project';
 export const GET_EMPLOYEE_DETAILS =
-  'http://127.0.0.1:8080/api/get/employee_details';
-export const ADD_NEW_EMPLOYEE = 'http://127.0.0.1:8080/api/post/employee';
-export const EDIT_A_EMPLOYEE = 'http://127.0.0.1:8080/api/put/employee';
-export const DELETE_A_EMPLOYEE = 'http://127.0.0.1:8080/api/delete/employee';
-export const GET_ALL_PAYMENTS = 'http://127.0.0.1:8080/api/get/payments';
+  'http://15.206.100.165:8080/api/get/employee_details';
+export const ADD_NEW_EMPLOYEE = 'http://15.206.100.165:8080/api/post/employee';
+export const EDIT_A_EMPLOYEE = 'http://15.206.100.165:8080/api/put/employee';
+export const DELETE_A_EMPLOYEE =
+  'http://15.206.100.165:8080/api/delete/employee';
+export const GET_ALL_PAYMENTS = 'http://15.206.100.165:8080/api/get/payments';
 export const GET_PAYMENT_DETAILS =
-  'http://127.0.0.1:8080/api/get/payment_details';
-export const ADD_NEW_PAYMENT = 'http://127.0.0.1:8080/api/post/payment';
-export const EDIT_A_PAYMENT = 'http://127.0.0.1:8080/api/put/payment';
-export const DELETE_A_PAYMENT = 'http://127.0.0.1:8080/api/delete/payment';
-export const GET_ALL_REVENUES = 'http://127.0.0.1:8080/api/get/revenues';
+  'http://15.206.100.165:8080/api/get/payment_details';
+export const ADD_NEW_PAYMENT = 'http://15.206.100.165:8080/api/post/payment';
+export const EDIT_A_PAYMENT = 'http://15.206.100.165:8080/api/put/payment';
+export const DELETE_A_PAYMENT = 'http://15.206.100.165:8080/api/delete/payment';
+export const GET_ALL_REVENUES = 'http://15.206.100.165:8080/api/get/revenues';
 export const GET_REVENUE_DETAILS =
-  'http://127.0.0.1:8080/api/get/revenue_details';
-export const ADD_NEW_REVENUE = 'http://127.0.0.1:8080/api/post/revenue';
-export const EDIT_A_REVENUE = 'http://127.0.0.1:8080/api/put/revenue';
-export const DELETE_A_REVENUE = 'http://127.0.0.1:8080/api/delete/revenue';
+  'http://15.206.100.165:8080/api/get/revenue_details';
+export const ADD_NEW_REVENUE = 'http://15.206.100.165:8080/api/post/revenue';
+export const EDIT_A_REVENUE = 'http://15.206.100.165:8080/api/put/revenue';
+export const DELETE_A_REVENUE = 'http://15.206.100.165:8080/api/delete/revenue';
 export const GET_ALL_EXPENSES_FOR_A_MONTH =
-  'http://127.0.0.1:8080/api/get/expenses/month';
+  'http://15.206.100.165:8080/api/get/expenses/month';
 export const GET_ALL_EXPENSES_FOR_A_PROJECT =
-  'http://127.0.0.1:8080/api/get/expenses/project';
+  'http://15.206.100.165:8080/api/get/expenses/project';
 export const GET_ALL_PAYMENTS_FOR_A_PROJECT =
-  'http://127.0.0.1:8080/api/get/payments/project';
+  'http://15.206.100.165:8080/api/get/payments/project';
 export const GET_ALL_REVENUES_FOR_A_SPECIFIC_PROJECT =
-  'http://127.0.0.1:8080/api/get/revenues/project';
+  'http://15.206.100.165:8080/api/get/revenues/project';
 export const DASHBOARD_DETAILS =
-  'http://127.0.0.1:8080/api/get/dashboard_details';
+  'http://15.206.100.165:8080/api/get/dashboard_details';
